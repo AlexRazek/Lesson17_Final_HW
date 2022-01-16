aa95464fc6c099a750180cf6ae5e710a2dd27224
alexander <Avvrazek@gmail.com>
2022-01-16 17:16:32 +0300

    Added two decorators: 1.For show all movies 2. For show movie by id

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+api = Api(app)
+movie_ns = api.namespace('movies')
+director_ns = api.namespace('directors')
+genre_ns = api.namespace('genres')
+
 
 class Movie(db.Model):
     __tablename__ = 'movie'
@@ -24,10 +29,41 @@ class Movie(db.Model):
     director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
     director = db.relationship("Director")
 
+class MovieSchema(Schema):
+    id = fields.Int(dump_only=True)
+    title = fields.Str()
+    description = fields.Str()
+
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
+
+
+# получаем все фильмы
+
+@movie_ns.route('/')
+class MovieView(Resource):
+    def get(self):
+        all_movies = Movie.query.all()
+        return movies_schema.dump(all_movies)
+
+# получаем фильм по id
+
+@movie_ns.route('/<int:id>')
+class MovieView(Resource):
+    def get(self, id):
+        movie = Movie.query.get(id)
+        return movie_schema.dump(movie), 200
+
+
 class Director(db.Model):
     __tablename__ = 'director'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
+
+class DirectorSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
 
 
 class Genre(db.Model):
@@ -35,6 +71,9 @@ class Genre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
 
+class GenreSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
 
 
 if __name__ == '__main__':
